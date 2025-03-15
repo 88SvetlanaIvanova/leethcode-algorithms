cf8d7cadce21473593f6a99c68c9c66be67e5ab0
88SvetlanaIvanova <96539287+88SvetlanaIvanova@users.noreply.github.com>
2025-03-15 16:22:05 +0200

    Create Group_Anagrams.java
    Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
    
    An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.

---
+++ Group_Anagrams.java
@@ -0,0 +1,10 @@
+ public List<List<String>> groupAnagrams(String[] strs) {
+         Map<String, List<String>> anagramMap = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sortedStr = new String(chars);
+            anagramMap.computeIfAbsent(sortedStr, k -> new ArrayList<>()).add(str);
+        }
+        return new ArrayList<>(anagramMap.values());
+    }
